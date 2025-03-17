d22c831244de042d5f573a70050436a7653694e4
LebFre1tas <lebfre1tasfsa@gmail.com>
2025-03-17 23:10:41 +0000

    Concluí o nível aventureiro do super trunfo.

---
+++ super_trunfo.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main(){
+    //Definição das variáveis da primeira carta.
+    char estado;
+    char codigo[20];
+    char cidade[20];
+    int populacao;
+    float area;
+    float PIB;
+    int turisticos;
+
+    //Definição das variáveis da segunda carta.
+    char estado1;
+    char codigo1[20];
+    char cidade1[20];
+    int populacao1;
+    float area1;
+    float PIB1;
+    int turisticos1;
+
+    //Cadastro da primeira carta.
+    printf("- Insira as informações da primeira carta: \n");
+
+    printf("Insira o Estado: \n");
+    scanf("%c", &estado);
+
+    printf("Insira o código da carta: \n");
+    scanf("%s", codigo);
+
+    printf("Insira a cidade: \n");
+    scanf("%s", cidade);
+
+    printf("Insira a população: \n");
+    scanf("%d", populacao);
+
+    printf("Insira a área (em km²): \n");
+    scanf("%f", area);
+
+    printf("Insira o PIB: \n");
+    scanf("%f", PIB);
+
+    printf("Insira o número de pontos turísticos: \n");
+    scanf("%d", turisticos);
+
+    //Cadastro da segunda carta.
+    printf("- Insira as informações da segunda carta: \n");
+
+
+
+
+
+
+
+
+
+
+
+}
